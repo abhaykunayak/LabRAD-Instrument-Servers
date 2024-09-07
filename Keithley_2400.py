79ef5d2f8d49a5c5b8cdcd9332d328238159a5fb
Abhay Kumar Nayak <abhaykunayak@gmail.com>
2024-09-07 14:17:15 -0700

    updated Keithley 2400 server from GP mod

--- Keithley_2400.py
+++ Keithley_2400.py
@@ -62,93 +62,94 @@ class K2400(GPIBManagedServer):
         current = yield dev.query('MEAS:CURR:DC?')
         current = (current.split(',')[1] )
         returnValue(float(current))
-    
+
     @setting(116)
     def output_on(self, c):
         dev = self.selectedDevice(c)
-        yield dev.write(':OUTP ON')     
+        yield dev.write(':OUTP ON')
         returnValue(1)
-        
+
     @setting(117)
     def output_off(self, c):
         dev = self.selectedDevice(c)
-        yield dev.write(':OUTP OFF')     
+        yield dev.write(':OUTP OFF')
         returnValue(1)
-        
+
     @setting(118, returns = 'v')
     def read_v(self, c):
         dev = self.selectedDevice(c)
-        data = yield dev.query('MEAS:CURR:DC?')   
-        volts = (data.split(',')[0] )  
+        data = yield dev.query('MEAS:CURR:DC?')
+        volts = (data.split(',')[0] )
         returnValue( float(volts) )
-        
+
     @setting(119, returns = 'v')
     def read_i(self, c):
         dev = self.selectedDevice(c)
         data = yield dev.query('MEAS:CURR:DC?')
-        current = (data.split(',')[1] )   
+        current = (data.split(',')[1] )
         returnValue( float(current) )
-    
+
     @setting(120, returns = [] )
-    def ramp_volt_SPCI(self,  c, start_num , stop_num, step_num, compl = 1e-6 ):        
-        dev = self.selectedDevice(c) 
+    def ramp_volt_SPCI(self,  c, start_num , stop_num, step_num, compl = 1e-6 ):
+        dev = self.selectedDevice(c)
         trace_point = 10
         yield dev.write('*RST')
         yield dev.write(':SENS:FUNC:CONC ON')
         yield dev.write(':SOUR:FUNC VOLT')
         yield dev.write(':SENSe:CURRent:PROTection ' + str(compl))
         yield dev.write(':SENS:FUNC "CURR"') #error in line
-        yield dev.write(':SOUR:VOLT:START '+str(start_num)) #  in volts bounds -200 to 200       
+        yield dev.write(':SOUR:VOLT:START '+str(start_num)) #  in volts bounds -200 to 200
         yield dev.write(':SOUR:VOLT:STOP '+str(stop_num))#  in volts bounds -200 to 200
         yield dev.write(':SOUR:VOLT:STEP '+str(step_num))#  in volts bounds -200 to 200
         trig_num = -(start_num - stop_num)/step_num +1
         yield dev.write(':SOUR:VOLT:MODE SWE')
-        yield dev.write(':SOUR:SWE:RANG AUTO')
+        #yield dev.write(':SOUR:SWE:RANG AUTO') # disabled 1/27 AMP
         yield dev.write(':SOUR:SWE:SPAC LIN')
         yield dev.write(':TRIG:COUN '+str(trig_num))
-        yield dev.write(':SOUR:DEL 0.1') #THIS IS THE AMOUNT OF DELAY 
-        #Storing the readings in buffer       
+        yield dev.write(':SOUR:DEL 0.1') #THIS IS THE AMOUNT OF DELAY
+        #Storing the readings in buffer
         yield dev.write(':TRAC:CLEar')
         yield dev.write(':TRAC:FEED SENS')
         yield dev.write(':TRAC:POIN '+str(trig_num))
         yield dev.write(':TRAC:FEED:CONT NEXT')
         yield dev.write(':TRIG:COUN '+str(trig_num))
-        yield dev.write(':OUTP ON') #MEASure OUTP ON  #running up to here       
+        yield dev.write(':OUTP ON') #MEASure OUTP ON  #running up to here
         yield dev.write(':INIT')
         print('Read complete')
-        time.sleep(trig_num*0.2) 
-        #sleep during measurment 
+        time.sleep(trig_num*0.2)
+        #sleep during measurment
         yield dev.write(':OUTP OFF')
-        num_values = yield dev.query(':TRAC:POIN:ACT?') #tells us how many measurments are there 
+        num_values = yield dev.query(':TRAC:POIN:ACT?') #tells us how many measurments are there
         data = yield dev.query(':TRAC:DATA?')
-        #pulls data 
+        #pulls data
         yield dev.write(':TRAC:CLE')
-        #current array 
+        #current array
         current = []
         x=0
         n=trig_num
         while x<n:
             current.append(float ( (data.split(',')[1+(x*5)])) )
             x= x+1
-        #voltage array 
+        #voltage array
         voltage = []
         j=0
         n=trig_num
         while j<n:
             voltage.append((data.split(',')[(j*5)]))
             j= j+1
-        print(current)
+        #print(current)
+        yield dev.write(':SYSTEM:KEY 23') # added AMP 6/17/22
         returnValue( (current) )
-        
+
     @setting(121, returns = [])
-    def ramp_current_SPCI(self,  c, start_num , stop_num, step_num, compl = 50e-6 ):        
-        dev = self.selectedDevice(c) 
+    def ramp_current_SPCI(self,  c, start_num , stop_num, step_num, compl = 50e-6 ):
+        dev = self.selectedDevice(c)
         yield dev.write('*RST')
         yield dev.write(':SENS:FUNC:CONC ON')
         yield dev.write(':SOUR:FUNC CURR')
         yield dev.write(':SENSe:CURR:PROTection ' + str(compl))
         yield dev.write(':SENS:FUNC "VOLT:DC"') #error in line
-        yield dev.write(':SOUR:CURR:START '+str(start_num)) #  in volts bounds -200 to 200       
+        yield dev.write(':SOUR:CURR:START '+str(start_num)) #  in volts bounds -200 to 200
         yield dev.write(':SOUR:CURR:STOP '+str(stop_num))#  in volts bounds -200 to 200
         yield dev.write(':SOUR:CURR:STEP '+str(step_num))#  in volts bounds -200 to 200
         trig_num = -(start_num - stop_num)/step_num +1
@@ -156,31 +157,31 @@ class K2400(GPIBManagedServer):
         yield dev.write(':SOUR:SWE:RANG AUTO')
         yield dev.write(':SOUR:SWE:SPAC LIN')
         yield dev.write(':TRIG:COUN '+str(trig_num))
-        yield dev.write(':SOUR:DEL 0.1')  #THIS IS THE AMOUNT OF DELAY 
-        #Storing the readings in buffer       
+        yield dev.write(':SOUR:DEL 0.1')  #THIS IS THE AMOUNT OF DELAY
+        #Storing the readings in buffer
         yield dev.write(':TRAC:CLEar')
         yield dev.write(':TRAC:FEED SENS')
         yield dev.write(':TRAC:POIN '+str(trig_num))
         yield dev.write(':TRAC:FEED:CONT NEXT')
         yield dev.write(':TRIG:COUN '+str(trig_num))
-        yield dev.write(':OUTP ON') #MEASure OUTP ON  #running up to here       
+        yield dev.write(':OUTP ON') #MEASure OUTP ON  #running up to here
         yield dev.write(':INIT')
         print('Read complete')
-        time.sleep(trig_num*0.2) 
-        #sleep during measurment 
+        time.sleep(trig_num*0.2)
+        #sleep during measurment
         yield dev.write(':OUTP OFF')
-        num_values = yield dev.query(':TRAC:POIN:ACT?') #tells us how many measurments are there 
+        num_values = yield dev.query(':TRAC:POIN:ACT?') #tells us how many measurments are there
         data = yield dev.query(':TRAC:DATA?')
-        #pulls data 
+        #pulls data
         yield dev.write(':TRAC:CLE')
-        #current array 
+        #current array
         current = []
         x=0
         n=trig_num
         while x<(n-1):
             current.append((data.split(',')[1+(x*5)]))
             x= x+1
-        #voltage array 
+        #voltage array
         voltage = []
         j=0
         n=trig_num
@@ -189,40 +190,40 @@ class K2400(GPIBManagedServer):
             j= j+1
 
         returnValue( voltage )
-     
-            
+
+
     @setting(122, returns = [])
-    def average_over_volt(self,  c, volt , trig_num , delay = 0.1, compl = 50e-6 ):        
+    def average_over_volt(self,  c, volt , trig_num , delay = 0.1, compl = 50e-6 ):
         dev = self.selectedDevice(c)
-        
+
         str_volts =  str(volt)+','
-        x = 2 
+        x = 2
         while x< trig_num:
             str_volts = str_volts + str(volt)+','
             x =x +1
         str_volts = str_volts + str(volt)
-    
+
         yield dev.write('*RST')
         yield dev.write(':TRAC:CLEar')
         yield dev.write(':SENS:FUNC:CONC ON')
         yield dev.write(':SOUR:FUNC VOLT')
         yield dev.write(':SENS:FUNC "CURR"')
         yield dev.write(':SENSe:CURR:PROTection ' + str(compl))
-        
+
         yield dev.write(':SOUR:VOLT:MODE LIST')
         yield dev.write(':SOUR:LIST:VOLT '+str_volts)
-        yield dev.write(':TRIG:COUN '+str(trig_num)) 
-        yield dev.write(':SOUR:DEL '+str(delay)) 
+        yield dev.write(':TRIG:COUN '+str(trig_num))
+        yield dev.write(':SOUR:DEL '+str(delay))
 
         yield dev.write(':TRAC:CLEar')
         yield dev.write(':TRAC:FEED SENS')
         yield dev.write(':TRAC:POIN '+str(trig_num))
         yield dev.write(':TRAC:FEED:CONT NEXT')
         yield dev.write(':TRIG:COUN '+str(trig_num))
-        yield dev.write(':OUTP ON') #MEASure OUTP ON  #running up to here       
+        yield dev.write(':OUTP ON') #MEASure OUTP ON  #running up to here
         yield dev.write(':INIT')
         print('Read complete')
-        time.sleep(trig_num*0.2) 
+        time.sleep(trig_num*0.2)
         data = yield dev.query(':TRACE:DATA?')
         yield dev.write(':OUTP OFF')
         yield dev.write(':CALC3:FORM MEAN')
@@ -234,39 +235,39 @@ class K2400(GPIBManagedServer):
 
 #        yield dev.write(':TRAC:CLEar')
         returnValue( [mean,standard_dev] )
-    
-    @setting(123, returns = [])    
-    def average_over_current(self,  c, current , trig_num , delay = 0.1, compl = 50e-6 ):        
+
+    @setting(123, returns = [])
+    def average_over_current(self,  c, current , trig_num , delay = 0.1, compl = 50e-6 ):
         dev = self.selectedDevice(c)
-        
+
         str_current =  str(current)+','
-        x = 2 
+        x = 2
         while x< trig_num:
             str_current = str_current + str(current)+','
             x =x +1
         str_current = str_current + str(current)
-       
+
         yield dev.write('*RST')
         yield dev.write(':TRAC:CLEar')
         yield dev.write(':SENS:FUNC:CONC ON')
         yield dev.write(':SOUR:FUNC CURR')
         yield dev.write(':SENS:FUNC "VOLT:DC"')
         yield dev.write(':SENSe:CURR:PROTection ' + str(compl))
-        
+
         yield dev.write(':SOUR:CURR:MODE LIST')
         yield dev.write(':SOUR:LIST:CURR '+str_current)
-        yield dev.write(':TRIG:COUN '+str(trig_num)) 
-        yield dev.write(':SOUR:DEL '+str(delay)) 
+        yield dev.write(':TRIG:COUN '+str(trig_num))
+        yield dev.write(':SOUR:DEL '+str(delay))
 
         yield dev.write(':TRAC:CLEar')
         yield dev.write(':TRAC:FEED SENS')
         yield dev.write(':TRAC:POIN '+str(trig_num))
         yield dev.write(':TRAC:FEED:CONT NEXT')
         yield dev.write(':TRIG:COUN '+str(trig_num))
-        yield dev.write(':OUTP ON') #MEASure OUTP ON  #running up to here       
+        yield dev.write(':OUTP ON') #MEASure OUTP ON  #running up to here
         yield dev.write(':INIT')
         print('Read complete')
-        time.sleep(trig_num*0.2) 
+        time.sleep(trig_num*0.2)
         data = yield dev.query(':TRACE:DATA?')
         yield dev.write(':OUTP OFF')
         yield dev.write(':CALC3:FORM MEAN')
@@ -277,8 +278,8 @@ class K2400(GPIBManagedServer):
         standard_dev = float(dev.split(',')[0])
 
 #        yield dev.write(':TRAC:CLEar')
-        returnValue( [mean,standard_dev] )    
-        
+        returnValue( [mean,standard_dev] )
+
 __server__ = K2400()
 
 if __name__ == '__main__':
--- RunServers.bat
+++ RunServers.bat
@@ -1,16 +1,29 @@
 @echo off
-REM Start labrad.bat
-start cmd.exe /k call C:\scalabrad-0.8.3\bin\labrad.bat
+REM Start labrad.bat minimized
+start /min cmd.exe /k call C:\scalabrad-0.8.3\bin\labrad.bat
 
 REM Define a list of Python scripts to run
-set scripts=serial_server.py dac_adc.py gpib_server.py gpib_device_manager.py ESP300.py sr860.py SR830.py data_vault.py Lakeshore350.py K2400_fromGP.py K2450_fromGP.py dataVaultLivePlotter.py
+set scripts=(
+	serial_server.py 
+	gpib_server.py 
+	gpib_device_manager.py 
+	dac_adc.py 
+	sr860.py 
+	SR830.py 
+	ESP302.py 
+	Lakeshore350.py 
+	K2400_fromGP.py 
+	K2450_fromGP.py 
+	data_vault.py 
+	dataVaultLivePlotter.py
+)
 
-REM Loop through each script and start it with a timeout
+REM Loop through each script and start it minimized with a timeout
 for %%s in (%scripts%) do (
-    REM Wait for 2 seconds before starting the next script
-    TIMEOUT 2 /NOBREAK
-    REM Start the Python script
-    start python C:\Software\afyservers-py3-master\%%s
+    REM Wait for 1 seconds before starting the next script
+    TIMEOUT 1 /NOBREAK
+    REM Start the Python script minimized
+    start /min python C:\Software\afyservers-py3-master\%%s
 )
 
 REM Pause the script to keep the command window open
