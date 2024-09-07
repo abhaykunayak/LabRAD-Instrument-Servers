8614be5c52dec2ecc320242606c5713c3fb59621
Abhay Kumar Nayak <abhaykunayak@gmail.com>
2024-09-07 14:53:53 -0700

    fixed bug in script path

--- RunServers.bat
+++ RunServers.bat
@@ -3,7 +3,7 @@ REM Specify the path to the Python executable
 set PYTHON_PATH=C:\Python39\python.exe
 
 REM Start labrad.bat minimized
-start cmd.exe /k call C:\scalabrad-0.8.3\bin\labrad.bat
+start /min cmd.exe /k call C:\scalabrad-0.8.3\bin\labrad.bat
 
 REM Define a list of Python scripts to run
 set scripts=serial_server.py gpib_server.py gpib_device_manager.py dac_adc.py sr860.py sr830.py ESP302.py Lakeshore350.py Keithley_2400.py Keithley_2450.py data_vault.py dataVaultLivePlotter.py
@@ -13,7 +13,7 @@ for %%s in (%scripts%) do (
     REM Wait for 1 seconds before starting the next script
     TIMEOUT 1 /NOBREAK
     REM Start the Python script minimized
-    start %PYTHON_PATH% C:\Software\afyservers-py3-master\%%s
+    start /min %PYTHON_PATH% "C:\Users\Marconi\Young Lab Dropbox\Young Group\THz\Scripts\LabRAD-Instrument-Servers\%%s"
 )
 
 REM Pause the script to keep the command window open
