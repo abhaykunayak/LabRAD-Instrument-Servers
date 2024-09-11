55a0106b603c18aa794af22ce1daff35ef64428f
Abhay Kumar Nayak <abhaykunayak@gmail.com>
2024-09-10 18:02:21 -0700

    ESP302 and Runservers
    Changed ESP300 to fit the ESP302 Server to function with sockets. Also changed the runservers file to run all the labrad servers.

--- ESP302.py
+++ ESP302.py
@@ -20,15 +20,15 @@ class ESP302Server(LabradServer):
         """
         self.esp_ip = '192.168.123.90'
         self.esp_port = 5001
-        self.timeout = 5  # Default timeout for socket operations in seconds
+        self.timeout = 1  # Default timeout for socket operations in seconds
         self.command_queue = Queue()  # Command queue
         self.sock = None
         self.reconnect()
 
-        # Start a thread for command processing
-        self.command_thread = Thread(target=self.process_commands)
-        self.command_thread.daemon = True
-        self.command_thread.start()
+        # # Start a thread for command processing
+        # self.command_thread = Thread(target=self.process_commands)
+        # self.command_thread.daemon = True
+        # self.command_thread.start()
 
     def reconnect(self):
         """
@@ -54,30 +54,47 @@ class ESP302Server(LabradServer):
             command_with_termination = command + '\r\n'
             self.sock.sendall(command_with_termination.encode())
             time.sleep(0.1)
-            response = self.sock.recv(1024).decode().strip()
-            logging.info(f"Sent: {command}, Received: {response}")
-            return response
         except socket.error as e:
             logging.error(f"Error sending command: {e}")
             self.reconnect()  # Attempt to reconnect if an error occurs
             return None
+        
+    def query_command(self, command):
+        """
+        Send a command to the ESP 302 and return its response.
+        """
+        try:
+            command_with_termination = command + '\r\n'
+            self.sock.sendall(command_with_termination.encode())
+            time.sleep(0.1)
+            response = self.sock.recv(1024).decode().strip()
+            logging.info(f"Sent: {command}, Received: {response}")
+            return response
+        except socket.error as e:
+            # Check for specific disconnection errors
+            if e.errno in (socket.errno.ECONNRESET, socket.errno.EPIPE):
+                logging.error(f"Disconnection error: {e}")
+                self.reconnect()  # Attempt to reconnect if a disconnection error occurs
+            else:
+                logging.error(f"Error sending command: {e}")
+            return "fail"
 
-    def process_commands(self):
-        """
-        Continuously process commands from the queue.
-        """
-        while True:
-            command, callback = self.command_queue.get()
-            response = self.send_command(command)
-            if callback:
-                callback(response)
-            self.command_queue.task_done()
+    # def process_commands(self):
+    #     """
+    #     Continuously process commands from the queue.
+    #     """
+    #     while True:
+    #         command, callback = self.command_queue.get()
+    #         response = self.send_command(command)
+    #         if callback:
+    #             callback(response)
+    #         self.command_queue.task_done()
 
-    def queue_command(self, command, callback=None):
-        """
-        Add a command to the command queue for processing.
-        """
-        self.command_queue.put((command, callback))
+    # def queue_command(self, command, callback=None):
+    #     """
+    #     Add a command to the command queue for processing.
+    #     """
+    #     self.command_queue.put((command, callback))
 
     # ========== New/Improved Functions ========== #
 
@@ -102,7 +119,24 @@ class ESP302Server(LabradServer):
         if not (0 <= bit <= 15):
             raise ValueError(f"Invalid DIO bit: {bit}. It must be between 0 and 15.")
 
-    @setting(10, 'Move Axis', axis='i', position='v', returns='s')
+    # @setting(9, 'tcp_query',message ='s', returns='s')
+    # def tcp_query(self, c, message):
+    #     command = f'{message}'
+    #     response = self.send_command(command)
+    #     return response
+    
+    # @setting(9, 'tcp_write',message ='s', returns='')
+    # def tcp_write(self, c, message):
+    #     command = f'{message}'
+    #     response = self.send_command(command)
+    
+    # @setting(9, 'tcp_read', returns='s')
+    # def tcp_read(self, c):
+    #     command = f''
+    #     response = self.send_command(command)
+    #     return response
+
+    @setting(10, 'Move Axis', axis='i', position='v', returns='')
     def move_axis(self, c, axis, position):
         """
         Move a specific axis to the given position.
@@ -112,8 +146,8 @@ class ESP302Server(LabradServer):
         position: float - The position to move the axis to.
         """
         command = f'{axis}PA{position}'
-        response = self.send_command(command)
-        return f'Move command response: {response}'
+        self.send_command(command)
+
 
     @setting(20, 'Get Position', axis='i', returns='v')
     def get_position(self, c, axis):
@@ -127,14 +161,14 @@ class ESP302Server(LabradServer):
         float - The current position of the axis.
         """
         command = f'{axis}TP'
-        response = self.send_command(command)
+        response = self.query_command(command)
         try:
             position = float(response)
-        except ValueError:
-            position = None
+        except TypeError:
+            position = -20000 #error code
         return position
 
-    @setting(30, 'Home Axis', axis='i', returns='s')
+    @setting(30, 'Home Axis', axis='i', returns='')
     def home_axis(self, c, axis):
         """
         Home a specific axis.
@@ -144,91 +178,101 @@ class ESP302Server(LabradServer):
         """
         command = f'{axis}OR'
         response = self.send_command(command)
-        return f'Home command response: {response}'
 
-    @setting(40, 'Stop All Motion', returns='s')
+    @setting(40, 'Stop All Motion', returns='')
     def stop_all_motion(self, c):
         """
         Stop all motion on all axes.
         """
         command = 'ST'
         response = self.send_command(command)
-        return f'Stop command response: {response}'
+        self.send_command(command)
 
-    # Move to Relative Position (PR)
-    @setting(100, 'Move Relative', axis='i', distance='v', returns='s')
-    def move_relative(self, c, axis, distance):
-        self.validate_axis(axis)
-        command = f'{axis}PR{distance}'
-        self.queue_command(command, lambda response: f'Move Relative response: {response}')
+
+    # # Move to Relative Position (PR)
+    # @setting(100, 'Move Relative', axis='i', distance='v', returns='')
+    # def move_relative(self, c, axis, distance):
+    #     self.validate_axis(axis)
+    #     command = f'{axis}PR{distance}'
+    #     self.send_command(command)
 
     # Stop Motion (ST)
-    @setting(110, 'Stop Motion', returns='s')
+    @setting(110, 'Stop Motion', returns='')
     def stop_motion(self, c):
         command = 'ST'
-        self.queue_command(command, lambda response: f'Stop motion response: {response}')
+        self.send_command(command)
 
-    # Read Error Code (TE)
-    @setting(120, 'Get Error Code', returns='s')
-    def get_error_code(self, c):
-        command = 'TE'
-        self.queue_command(command, lambda response: f'Error code: {response}')
+    # # Read Error Code (TE)
+    # @setting(120, 'Get Error Code', returns='s')
+    # def get_error_code(self, c):
+    #     command = 'TE'
+    #     response = self.query_command(command)
+    #     return f'Error message: {response}'
 
-    # Read Error Message (TB)
-    @setting(130, 'Get Error Message', returns='s')
-    def get_error_message(self, c):
-        command = 'TB'
-        self.queue_command(command, lambda response: f'Error message: {response}')
+    # # Read Error Message (TB)
+    # @setting(130, 'Get Error Message', returns='s')
+    # def get_error_message(self, c):
+    #     command = 'TB'
+    #     response = self.query_command(command)
+    #     return f'Error message: {response}'
+
+    # #Wait Stop
+    @setting(140, 'Wait Stop', axis='i', milliseconds='v', returns='')
+    def wait_stop(self, c, axis, milliseconds):
+        self.validate_axis(axis)
+        command = f'{axis}WS{milliseconds}'
+        self.send_command(command)
+
 
     # Set Velocity (VA)
-    @setting(150, 'Set Velocity', axis='i', velocity='v', returns='s')
+    @setting(150, 'Set Velocity', axis='i', velocity='v', returns='')
     def set_velocity(self, c, axis, velocity):
-        self.validate_axis(axis)
-        self.validate_velocity(velocity)
+        # self.validate_axis(axis)
+        # self.validate_velocity(velocity)
         command = f'{axis}VA{velocity}'
-        self.queue_command(command, lambda response: f'Set velocity response: {response}')
+        self.send_command(command)
 
     # Set Acceleration (AC)
-    @setting(160, 'Set Acceleration', axis='i', acceleration='v', returns='s')
+    @setting(160, 'Set Acceleration', axis='i', acceleration='v', returns='')
     def set_acceleration(self, c, axis, acceleration):
         self.validate_axis(axis)
         command = f'{axis}AC{acceleration}'
-        self.queue_command(command, lambda response: f'Set acceleration response: {response}')
+        self.send_command(command)
 
     # Motor On (MO)
-    @setting(170, 'Motor On', axis='i', returns='s')
+    @setting(170, 'Motor On', axis='i', returns='')
     def motor_on(self, c, axis):
         self.validate_axis(axis)
         command = f'{axis}MO'
-        self.queue_command(command, lambda response: f'Motor on response: {response}')
+        self.send_command(command)
 
     # Set Deceleration (AG)
-    @setting(180, 'Set Deceleration', axis='i', deceleration='v', returns='s')
+    @setting(180, 'Set Deceleration', axis='i', deceleration='v', returns='')
     def set_deceleration(self, c, axis, deceleration):
         self.validate_axis(axis)
         command = f'{axis}AG{deceleration}'
-        self.queue_command(command, lambda response: f'Set deceleration response: {response}')
+        self.send_command(command)
 
     # Assign DIO Bits for Motion Status (BM)
-    @setting(190, 'Assign DIO Bits Motion Status', axis='i', bit_num='i', bit_level='i', returns='s')
+    @setting(190, 'Assign DIO Bits Motion Status', axis='i', bit_num='i', bit_level='i', returns='')
     def assign_dio_motion_status(self, c, axis, bit_num, bit_level):
         self.validate_axis(axis)
         self.validate_dio_bit(bit_num)
         command = f'{axis}BM{bit_num},{bit_level}'
-        self.queue_command(command, lambda response: f'Assign DIO bits motion status response: {response}')
+        self.send_command(command)
 
     # Enable DIO Bits for Motion Status (BN)
-    @setting(200, 'Enable DIO Bits Motion Status', axis='i', enable='b', returns='s')
+    @setting(200, 'Enable DIO Bits Motion Status', axis='i', enable='b', returns='')
     def enable_dio_motion_status(self, c, axis, enable):
         self.validate_axis(axis)
         command = f'{axis}BN{1 if enable else 0}'
-        self.queue_command(command, lambda response: f'Enable DIO bits motion status response: {response}')
+        self.send_command(command)
 
     # Set DIO Port Direction (BO)
-    @setting(210, 'Set DIO Port Direction', direction='s', returns='s')
+    @setting(210, 'Set DIO Port Direction', direction='s', returns='')
     def set_dio_port_direction(self, c, direction):
         command = f'BO{direction}'
-        self.queue_command(command, lambda response: f'Set DIO port direction response: {response}')
+        self.send_command(command)
 
     def stopServer(self):
         """
--- RunServers.bat
+++ RunServers.bat
@@ -2,18 +2,44 @@
 REM Specify the path to the Python executable
 set PYTHON_PATH=C:\Python39\python.exe
 
+REM Specify the directory containing the Python scripts
+set SCRIPT_DIR=C:\Users\Marconi\Young Lab Dropbox\Young Group\THz\Scripts\LabRAD-Instrument-Servers
+
+REM Specify the timeout duration (in seconds)
+set TIMEOUT_DURATION=1
+
+REM Log file path
+set LOG_FILE=%SCRIPT_DIR%\script_log.txt
+
 REM Start labrad.bat minimized
 start /min cmd.exe /k call C:\scalabrad-0.8.3\bin\labrad.bat
 
 REM Define a list of Python scripts to run
 set scripts=serial_server.py gpib_server.py gpib_device_manager.py dac_adc.py sr860.py sr830.py ESP302.py Lakeshore350.py Keithley_2400.py Keithley_2450.py data_vault.py dataVaultLivePlotter.py
 
+REM Create or clear the log file
+echo Script Execution Log > %LOG_FILE%
+echo ==================== >> %LOG_FILE%
+
 REM Loop through each script and start it minimized with a timeout
 for %%s in (%scripts%) do (
-    REM Wait for 1 seconds before starting the next script
-    TIMEOUT 1 /NOBREAK
-    REM Start the Python script minimized
-    start /min %PYTHON_PATH% "C:\Users\Marconi\Young Lab Dropbox\Young Group\THz\Scripts\LabRAD-Instrument-Servers\%%s"
+    REM Check if the script exists
+    if exist "%SCRIPT_DIR%\%%s" (
+        REM Log the start time
+        echo Starting %%s at %time% >> %LOG_FILE%
+        
+        REM Wait for the specified timeout duration before starting the next script
+        TIMEOUT %TIMEOUT_DURATION% /NOBREAK
+        
+        REM Start the Python script minimized
+        start /min %PYTHON_PATH% "%SCRIPT_DIR%\%%s"
+        
+        REM Log the end time
+        echo Started %%s at %time% >> %LOG_FILE%
+    ) else (
+        REM Log the error
+        echo ERROR: Script %%s not found at %time% >> %LOG_FILE%
+    )
 )
 
 REM Pause the script to keep the command window open
